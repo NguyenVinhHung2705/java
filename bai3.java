51ebd1af3b6e92c5285647c1ad3159bf6e9dfa1c
Nguyễn Vĩnh Hưng <150142110+NguyenVinhHung2705@users.noreply.github.com>
2025-03-20 21:27:46 +0700

    .

--- bai3.java
+++ bai3.java
@@ -10,16 +10,21 @@ public class bai3 {
 			System.out.println("Nhập vào một số:");
 			x = s.nextInt();
 		} while(x <= 1000);
-		int count = 0, sum = 0;
+		int sum = 0;
 		System.out.print("Số " + x + " ");
+		boolean flag = false;
 		while(x > 0) {
 			if(x % 10 == 5) {
-				++count;
+				flag = true;
 			}
 			sum += x % 10;
 			x/=10;
 		}
+		if(flag) {
+			System.out.println("có chứa số 5 và có tổng là: " + sum);
+		}else {
+			System.out.println("không chứa số 5 và có tổng là: " + sum);
+		}
 		
-		System.out.println("có "+ count+ " số 5 và có tổng là: " + sum);
 	}
 }
