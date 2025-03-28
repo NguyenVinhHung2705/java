ca61554cf367721341f619d8dab9669daecddd90
Nguyễn Vĩnh Hưng <150142110+NguyenVinhHung2705@users.noreply.github.com>
2025-03-28 08:26:40 +0700

    update

--- bai5.java
+++ bai5.java
@@ -7,16 +7,12 @@ public class bai5 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int x;
-		boolean isPrime;
 		int i;
 		do {
 			System.out.println("Nhập vào một số: ");
 			x = s.nextInt();
-			isPrime = true;
-			
 			for(i = 2;i*i<=x;i++) {
 				if(x % i == 0) {
-					isPrime = false;
 					break;
 				}
 			}
